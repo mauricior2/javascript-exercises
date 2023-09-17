2893ddacd81e0335fd3501a0933abb802b1b2257
Mauricio Ruiz <emauricio.ruizr@gmail.com>
2023-09-16 20:14:47 -0500

    Add

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+    let splitString  = word.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray    = reverseArray.join("");
+
+    return joinArray;
+
 
 };
 
