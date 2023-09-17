c96ef176d8f80c0016a2fb52fd313c8ae251dab5
Mauricio Ruiz <emauricio.ruizr@gmail.com>
2023-09-16 21:01:14 -0500

    Add

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(from , to) {
 
+    let sumTotal = 0;
+
+    if (from < 0 || to < 0 || !Number.isInteger(from) || !Number.isInteger(to)){
+
+        return 'ERROR';
+    }
+
+
+    if (from > to){
+        for(let i = to ; i <= from ; i++){
+            sumTotal += i;
+
+    }
+}
+
+    for(let i = from ; i <= to ; i++){
+
+        sumTotal += i;
+        
+    }
+    return sumTotal;
 };
 
 // Do not edit below this line
