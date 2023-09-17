f1556330120d4b0ab3a80a585c2f9f313398e332
Mauricio Ruiz <emauricio.ruizr@gmail.com>
2023-09-16 21:36:58 -0500

    Add

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number1) {
+  let celsius = Math.round((number1 - 32) * (5 / 9) * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number2) {
+  let fahrenheit = Math.round((number2 * ( 9 / 5) + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
